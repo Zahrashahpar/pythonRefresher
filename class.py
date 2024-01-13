acc70275d58a9d6b9c65451b067f4f98344f437e
Zshahpar <seifishahpar.zs@gmail.com>
2024-01-13 13:39:01 +0330

    added gender to student

--- class.py
+++ class.py
@@ -1,14 +1,18 @@
 class Student:
-    def __init__(self, name, age, major):
+    def __init__(self, name, age, major, gender):
         self.name = name
         self.age = age
         self.major = major
+        self.gender = gender
 
     def change_major(self, new_major):
         self.major = new_major
 
     def __str__(self):
-        return f"Name: {self.name}\nAge: {self.age}\nMajor: {self.major}"
+        if self.gender == "male":
+            return f"He is {self.name}\nAge: {self.age}\nMajor: {self.major}"
+        else  
+            return f"She is {self.name}\nAge: {self.age}\nMajor: {self.major}"
 
 
 s1 = Student("Zahra", 20, "Computer Engineering")
